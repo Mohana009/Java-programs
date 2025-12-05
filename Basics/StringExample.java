b9c216c0284a9e48a73540e26eb3ceb92677b2a5
Mohana <mohanaseeramsetti@gmail.com>
2025-12-05 17:48:14 +0530

    Add files via upload

---
+++ Basics/StringExample.java
@@ -0,0 +1,8 @@
+class StringExample{
+	public static void main(String[] args){
+		String name = "Alice";
+		System.out.println(name.charAt(1));
+		System.out.println(name.charAt(4));
+		System.out.println(name.length());
+	}
+}
